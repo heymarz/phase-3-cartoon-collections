a030a328f1bfec7c86be7ed41cb85c9d2030de56
Marlena Tsang <Marlena345@gmail.com>
2022-01-24 14:13:36 -0500

    commit tree b5c16fd412819a1c07caff6dbab6f83f95dbb1fb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#prints out the 7 dwarfs in a numbered list 
+def roll_call_dwarves(dwarfs)
+  dwarfs.each.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls)
+  planteer_calls.map do |call|
+    call.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+  call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
